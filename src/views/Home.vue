8cdc34e6f93f6e2855090d9bb9d1cbfb16babaef
niedzwiedzw <niedzwiedzwo@gmail.com>
2020-03-14 17:48:04 +0100

    looks fine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Home">
+    Hello, world
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import {defineComponent} from '@vue/composition-api';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+    name: "Home",
+})
 </script>
+
+<style scoped lang="scss">
+@import "src/styles/main";
+
+.Home {
+
+}
+</style>
